06369051079a0611e1c8967dec665ccf6720db88
AnalistaNiif <auxiliarniif@AnalistaNiif-PC.Serviport.com>
2018-01-29 13:32:07 -0500

    creando paneles

---
+++ src/components/nomina/nmPanelNovedades.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="nmPanel">
+    <table>
+      <thead>
+        <tr>
+          <td>Identificacion</td>
+          <td>Empleado</td>
+          <td>Salario</td>
+          <td>HRN</td>
+          <td>HED</td>
+          <td>HEN</td>
+          <td>HDF</td>
+        </tr>
+      </thead>
+      <tr>
+        <td>Identificacion</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+ export default {
+   data() {
+     return {
+       items:[]
+     }
+   },
+   created(){
+     this.FectchItems();
+   },
+   methods: {
+     FectchItems(){
+       this.axios.Get('/items')
+       .then(res => console.log(res))
+       .catch(err => console.log(err));
+     }
+   }
+ }
+</script>
